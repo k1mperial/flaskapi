1d9e0b8631e2719ab548c2ce9edae034238b3ece
root <root@ip-172-31-41-120.us-east-2.compute.internal>
2020-01-10 22:22:51 +0000

    Added Flask Blueprints

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,10 +1,11 @@
 import pytest
 
-from project import app, db
+from project import create_app, db  # updated
 
 
 @pytest.fixture(scope='module')
 def test_app():
+    app = create_app()  # new
     app.config.from_object('project.config.TestingConfig')
     with app.app_context():
         yield app  # testing happens here
